54059482952df042b00e50f9f3414643480aeaa9
Agung RB <agungrbudiman@gmail.com>
2023-07-10 09:08:02 +0700

    add clean sinf support

--- modules/clean_resource.js
+++ modules/clean_resource.js
@@ -7,15 +7,25 @@ const args = process.argv.slice(2);
 try {
   const data = fs.readFileSync(args[0], 'utf8');
   var json = plist.parse(data);
-  // console.log(json.files);
 } catch (err) {
   console.error(err);
 }
 
-for (let x in json) {
-  for (let y in json[x]) {
-    if (y.includes('PlugIns') || y.includes('WatchApp.app')){
-      delete json[x][y];
+try {
+  for (let x in json) {
+    for (let [i, y] of json[x].entries()) {
+      if (y.includes('PlugIns')){
+        delete json[x][i];
+      }
+    }
+  }
+}
+catch {
+  for (let x in json) {
+    for (let y in json[x]) {
+      if (y.includes('PlugIns') || y.includes('WatchApp.app')){
+        delete json[x][y];
+      }
     }
   }
 }
