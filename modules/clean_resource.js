7bcc2f040b81cea978d17f899718e4fcc3538f6f
Agung RB <agungrbudiman@gmail.com>
2023-07-10 09:14:45 +0700

    fix error handling

--- modules/clean_resource.js
+++ modules/clean_resource.js
@@ -7,32 +7,31 @@ const args = process.argv.slice(2);
 try {
   const data = fs.readFileSync(args[0], 'utf8');
   var json = plist.parse(data);
-} catch (err) {
-  console.error(err);
-}
-
-try {
-  for (let x in json) {
-    for (let [i, y] of json[x].entries()) {
-      if (y.includes('PlugIns')){
-        delete json[x][i];
+  try {
+    for (let x in json) {
+      for (let [i, y] of json[x].entries()) {
+        if (y.includes('PlugIns')){
+          delete json[x][i];
+        }
       }
     }
   }
-}
-catch {
-  for (let x in json) {
-    for (let y in json[x]) {
-      if (y.includes('PlugIns') || y.includes('WatchApp.app')){
-        delete json[x][y];
+  catch {
+    for (let x in json) {
+      for (let y in json[x]) {
+        if (y.includes('PlugIns') || y.includes('WatchApp.app')){
+          delete json[x][y];
+        }
       }
     }
   }
-}
 
-try {
-  fs.writeFileSync(args[0], plist.build(json));
-  // file written successfully
-} catch (err) {
-  console.error(err);
+  try {
+    fs.writeFileSync(args[0], plist.build(json));
+    // file written successfully
+  } catch (err) {
+    console.error(err);
+  }
 }
+catch {}
+
